67f7d057af1973b627c75d25e427f2d36a84b896
eyawson <ebow.yawson@gmail.com>
2020-07-26 17:12:29 -0400

    add main school info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = (props) => (
                 display: 'none'
             } : {}
         } >
-        <p className="copyright" > &copy; Moba '94 Design: <a href="https://ebow.me" target="_blank">Ebow Yawson</a>. Built with: <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby.js</a></p>
+        <p className="copyright" > &copy; Moba'94 Design: <a href="https://ebow.me" target="_blank">Ebow Yawson</a>. <a href="https://en.wikipedia.org/wiki/Mfantsipim_School" target="_blank">Mfantsipim School</a>. Built with: <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby.js</a></p>
     </footer>
 )
 
